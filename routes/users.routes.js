4e5ea875a1812f7a1c941832b99f7f60e7bfe3ba
Roberto <roberto.m.aragon@gmail.com>
2019-02-06 16:38:11 +0100

    minum fixes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -5,16 +5,30 @@ const usersController = require('../controllers/users.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 const userMiddleware = require('../middlewares/user.middleware');
 
-router.get('/', authMiddleware.isAuthenticated, usersController.main);
+router.get('/',
+  authMiddleware.isAuthenticated,
+  usersController.main);
+
 router.get('/list',
   authMiddleware.isAuthenticated,
   authMiddleware.checkRole(constants.ROLE_ADMIN),
-  usersController.list)
+  usersController.list);
+
 router.get('/create',
   authMiddleware.isAuthenticated,
   userMiddleware.isRegistered,
   usersController.create);
-router.post('/create', usersController.doCreate)
+
+router.post('/create',
+  usersController.doCreate)
+
+router.get('/edit',
+  authMiddleware.isAuthenticated,
+  usersController.edit);
+
+// router.post('/edit',
+//   authMiddleware.isAuthenticated,
+//   usersController.doEdit);
 
 router.get('/main',
   authMiddleware.isAuthenticated,
