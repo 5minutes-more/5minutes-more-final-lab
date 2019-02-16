8f994e36a4b62a12a0be24a3b2069edc8e489517
Roberto <roberto.m.aragon@gmail.com>
2019-02-16 18:12:50 +0100

    bug fixed

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -77,7 +77,7 @@ module.exports.doPay = (req,res, next) => {
   // Set your secret key: remember to change this to your live secret key in production
 // See your keys here: https://dashboard.stripe.com/account/apikeys
 var stripe = require("stripe")("sk_test_JBEiTzcKQiURs19EBdULsyNj");
-console.log("params => ",req.params)
+console.log("params => ", req.params)
 
 // Token is created using Checkout or Elements!
 // Get the payment token ID submitted by the form:
@@ -86,15 +86,15 @@ Order.findById(req.params.orderId)
   .populate('user')
   .populate('bar')
   .then(order => {
-    (async () => {
-      const charge = await stripe.charges.create({
-        amount: order.total,
-        currency: 'eur',
-        description: 'Example charge',
-        source: token,
-      })
-      console.log('charge', charge);
-    })();
+    // (async () => {
+    //   const charge = await stripe.charges.create({
+    //     amount: order.total,
+    //     currency: 'eur',
+    //     description: 'Example charge',
+    //     source: token,
+    //   })
+    //   console.log('charge', charge);
+    // })();
     res.render('orders/completed', { order })
   })
   .catch(error => res.render('orders/error', { error }))
